3ebc022f8d649ea37e8a2506c3d79ed7a4fe3f42
Benjie Manalo <Benjie.Manalo@ey.com>
2023-02-17 07:32:41 +0800

    initial commit for assignment 01

---
+++ Assignment_01/Assignment_01/Helper/BmmHeader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_01
+{
+    public static class BmmHeader
+    {
+        public static void Write()
+        {
+            Console.WriteLine("Author: Benjie Manalo");
+            Console.WriteLine($"Date: {DateTime.Now}");
+            Console.WriteLine("This is application is a video rental application wherein the user can view and rent videos based on titles.");
+            Console.WriteLine("**********************************");
+            Console.WriteLine("VIDEO TITLES and YEAR Released");
+            Console.WriteLine("**********************************");
+        }
+    }
+}
---
+++ Assignment_01/Assignment_01/Videos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Assignment_01
+{
+    public class BmmAvailableVideos
+    {
+        public List<BmmVideoEventArgs>? Videos { get; set; } = GetVideos();
+
+        private static List<BmmVideoEventArgs>? GetVideos()
+        {
+            string dir = Path.Join(Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName, @"Data\Videos.json");
+            string videoFile = File.ReadAllText(dir);
+            return JsonSerializer.Deserialize<List<BmmVideoEventArgs>>(videoFile);
+        }
+
+        public BmmVideoEventArgs GetVideo(string title)
+        {
+            return Videos?.FirstOrDefault(_ => _.Title == title);
+        }
+    }
+}
