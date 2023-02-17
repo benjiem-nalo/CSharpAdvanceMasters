f572187d0202fe03151894b9097f09913f965451
benjiem-nalo <90720756+benjiem-nalo@users.noreply.github.com>
2023-02-17 20:38:59 +0800

    checkin session 1 and 2 assignment (#1)
    * checkin session 1 and 2 assignment
    
    * minor changes
    
    ---------
    
    Co-authored-by: Benjie Manalo <Benjie.Manalo@ey.com>

---
+++ Assignment_01/Assignment_01/Helper/BmmHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_01
+{
+    public static class BmmHelper
+    {
+        public static string Categorize(this int year)
+        {
+            return year < 2023 ? "Old" : "New-Released";
+        }
+    }
+}
---
+++ Assignment_01/Assignment_01/Services/BmmMailService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Assignment_01
+{
+    public class BmmMailService
+    {
+        public static void Send(object? sender, BmmVideoEventArgs e)
+        {
+            var fromEmail = new MailAddress("benjie.manalo@softvision.com", "Benjie");
+            var toEmail = new MailAddress("gjgceredon@gmail.com", "Proctor");
+            //Note: This is a work email password. Please reach out to me if needed.
+            const string fromPassword = "";
+            const string subject = "Video Out";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = $"Email: {e.Year.Categorize()} video with title {e.Title} was rented."
+            })
+            {
+                smtp.Send(message);
+            }
+
+            Console.WriteLine($"Email sent to {toEmail} with email body [ {e.Year.Categorize()} video with title {e.Title} was rented. ]");
+        }
+    }
+}
