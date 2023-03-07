155765cdfa739a4cd324cad01fce5dc31eb04510
Benjie Manalo <Benjie.Manalo@ey.com>
2023-03-07 23:41:14 +0800

    checkin assignment 3

---
+++ Assignment_03/Assignment_03/InputCollections.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_03
+{
+    public class InputCollections
+    {
+        private Dictionary<int, object?> repo;
+
+        public InputCollections(Dictionary<int, object?> _repo)
+        {
+            repo = _repo;
+        }
+        public List<string>? StringList { get; set; } = new();
+        public List<int>? IntList { get; set; } = new();
+        public List<bool>? BoolList { get; set; } = new();
+        public List<double>? DoubleList { get; set; } = new();
+
+        public void AddInput<T>(T input)
+        {
+            //ParseAndSort(repo);
+            var s = input?.ToString();
+            if (!string.IsNullOrWhiteSpace(s))
+            {
+                repo?.Add(repo.Count + 1, input);
+
+                if (int.TryParse(s, out int f))
+                {
+                    IntList.Add(f);
+                }
+                else if (double.TryParse(s, out double d))
+                {
+                    DoubleList.Add(d);
+                }
+                else if (bool.TryParse(s, out bool b))
+                {
+                    BoolList.Add(b);
+                }
+                else
+                {
+                    StringList.Add(s);
+                }
+            }
+
+        }
+
+        public void DisplayList()
+        {
+            Console.WriteLine("All inputs:");
+            repo.ToList().ForEach(i => Console.WriteLine($"{i.Key} : {i.Value}"));
+            Console.WriteLine($"Int inputs");
+            IntList.ForEach(i => Console.WriteLine($"{i}"));
+            Console.WriteLine($"Double inputs");
+            DoubleList.ForEach(i => Console.WriteLine($"{i}"));
+            Console.WriteLine($"Bool inputs");
+            BoolList.ForEach(i => Console.WriteLine($"{i}"));
+            Console.WriteLine($"String inputs");
+            StringList.ForEach(i => Console.WriteLine($"{i}"));
+        }
+
+        public void ClearLists()
+        {
+            repo.Clear();
+            IntList.Clear();
+            DoubleList.Clear();
+            BoolList.Clear();
+            StringList.Clear();
+        }
+    }
+}
---
+++ Assignment_03/Assignment_03/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Assignment_03;
+
+Dictionary<int, object> keyValuePairs = new();
+Console.WriteLine("Add item in the list!!!");
+InputCollections inputCollections = new InputCollections(keyValuePairs);
+ConsoleKey key;
+do
+{
+    while (!Console.KeyAvailable)
+    {
+        inputCollections.AddInput(Console.ReadLine());
+
+        Console.WriteLine($"Add more? Y/N");
+        if (Console.ReadKey(true).Key == ConsoleKey.N)
+        {
+            inputCollections.DisplayList();
+            inputCollections.ClearLists();
+        }
+        else
+        {
+            inputCollections.AddInput(Console.ReadLine());
+           
+        }
+    }
+
+    // Key is available - read it
+    key = Console.ReadKey(true).Key;
+
+    if (key == ConsoleKey.Z)
+    {
+        //inputCollections.DisplayList();
+    }
+
+} while (key != ConsoleKey.Escape);
